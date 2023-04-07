3a15ca53b2c8bee8c4bb59097dc720e960f88e79
Madd-G <akhmadnuralamsyah@gmail.com>
2023-04-07 13:56:51 +0700

    router settings

--- lib/ui/home_screen/screen/home_screen.dart
+++ lib/ui/home_screen/screen/home_screen.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:nutria/utils/constants.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.pushNamed(context, profileRoute);
+          },
+          child: const Text('Home Screen'),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/utils/constants.dart
@@ -0,0 +1,4 @@
+// This file contains all the routing constants used within the app
+const String homeRoute = '/';
+const String profileRoute = '/profile';
+const String scanRoute = '/scan';
