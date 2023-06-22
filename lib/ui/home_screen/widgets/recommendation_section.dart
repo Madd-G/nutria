80e6af6632505318c0166114fb514df43f261b55
Madd-G <akhmadnuralamsyah@gmail.com>
2023-06-22 20:14:21 +0700

    feat: Get recommendation data from firestore

--- lib/ui/home_screen/widgets/recommendation_section.dart
+++ lib/ui/home_screen/widgets/recommendation_section.dart
@@ -1,5 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import '../../../blocs/blocs.dart';
+import 'package:nutria/ui/home_screen/widgets/widgets.dart';
 
 class RecommendationSection extends StatelessWidget {
   const RecommendationSection({
@@ -34,118 +35,41 @@ class RecommendationSection extends StatelessWidget {
             ),
           ],
         ),
-        const RecommendationCarousel()
+        StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('items')
+              .orderBy('viewed', descending: true)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            if (snapshot.data!.docs.isEmpty) {
+              return const SizedBox(
+                child: Center(child: Text("Not found")),
+              );
+            }
+            if (snapshot.hasData) {
+              final List<DocumentSnapshot> documents = snapshot.data!.docs;
+              return SizedBox(
+                height: 180,
+                child: ListView.builder(
+                  reverse: false,
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 6,
+                  itemBuilder: (BuildContext context, int index) {
+                    var doc = documents[index];
+                    return RecommendationCard(doc: doc);
+                  },
+                ),
+              );
+            } else {
+              return const SizedBox();
+            }
+          },
+        ),
       ],
-    );
-  }
-}
-
-class RecommendationCarousel extends StatelessWidget {
-  const RecommendationCarousel({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<DataBloc, DataState>(
-      builder: (context, state) {
-        if (state is LoadingState) {
-          return const SizedBox(
-            height: 150.0,
-            child: Center(
-              child: CircularProgressIndicator(),
-            ),
-          );
-        } else if (state is ErrorState) {
-          return SizedBox(
-            height: 150.0,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('An error occurred'),
-                const SizedBox(
-                  height: 10.0,
-                ),
-                IconButton(
-                  onPressed: () =>
-                      context.read<DataBloc>().add(GetRecommendations()),
-                  icon: Icon(
-                    Icons.refresh,
-                    color: Theme.of(context).colorScheme.primary,
-                    size: 30,
-                  ),
-                ),
-              ],
-            ),
-          );
-        } else if (state is RecommendationsSuccessState) {
-          return SizedBox(
-            height: 180,
-            child: ListView.builder(
-              reverse: false,
-              shrinkWrap: true,
-              scrollDirection: Axis.horizontal,
-              itemCount: 6,
-              itemBuilder: (BuildContext context, int index) {
-                return GestureDetector(
-                  onTap: () {},
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Stack(
-                      children: <Widget>[
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(10.0),
-                          child: GestureDetector(
-                            onTap: () {},
-                            child: const Image(
-                              height: 250.0,
-                              width: 250.0,
-                              image: AssetImage('assets/images/fruit.png'),
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                        Positioned(
-                          left: 5.0,
-                          bottom: 15.0,
-                          child: Stack(
-                            children: [
-                              Container(
-                                width: 70,
-                                height: 25,
-                                // color: Colors.white,
-                                decoration: const BoxDecoration(
-                                  color: Colors.white54,
-                                  borderRadius: BorderRadius.only(
-                                    topRight: Radius.circular(5.0),
-                                    bottomRight: Radius.circular(5.0),
-                                  ),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.fromLTRB(5, 3, 0, 0),
-                                child: Text(
-                                  '${state.recommendations.data?[index].name}',
-                                  style: const TextStyle(
-                                    color: Colors.black26,
-                                    fontSize: 20.0,
-                                    fontWeight: FontWeight.w600,
-                                    letterSpacing: 1.2,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            ),
-          );
-        } else {
-          return const SizedBox();
-        }
-      },
     );
   }
 }
